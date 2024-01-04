8dfdb18e9533fbda38797b072d61de7391a682d8
NLJKAM <nljbero540@gmail.com>
2024-01-04 18:42:24 +0900

    플레이어(검) 공격, 타겟기능 추가

--- Assets/Scripts/JHS/MainSceneMonster.cs
+++ Assets/Scripts/JHS/MainSceneMonster.cs
@@ -5,22 +5,63 @@ using UnityEngine;
 public class MainSceneMonster : MonoBehaviour
 {
     public float speed;
+    private Vector3 playerPos;
+    public bool move;
+    public float monsterHP;
+    public bool hit;
+    private SpriteRenderer spr;
+    private void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();
+    }
     void Start()
     {
-        
+        playerPos = new Vector3(0, 0, 0);
+        playerPos -= transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-speed*Time.deltaTime, 0, 0);
+        MonsterMove();
+        MonsterHit();
+        MonsterDie();
+    }
+    public void MonsterDie()
+    {
+        if (monsterHP <= 0)
+        {
+            Sword.instance.monstersLockOnQueue.Remove(gameObject);
+            Destroy(gameObject);
+        }
+    }
+    private void MonsterHit()
+    {
+        if (hit)
+        {
+            hit = false;
+            StartCoroutine("HitEffect");
+        }
+    }
+    IEnumerator HitEffect()
+    {
+        spr.color = Color.red;
+        yield return new WaitForSeconds(0.05f);
+        spr.color = Color.white;
+    }
+    private void MonsterMove()
+    {
+        if (move)
+        {
+            transform.position += speed * playerPos * Time.deltaTime * 0.2f;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            SwordInformation.instance.currentFarmingGaug++;
-            Destroy(gameObject);
+            Sword.instance.currentFarmingGauge++;
+            move = false;
+            Sword.instance.LockOnQueue(gameObject);
         }
     }
 }
