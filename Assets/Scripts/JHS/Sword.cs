b522361ac6f700215f25a273705dab8c214652e3
NLJKAM <nljbero540@gmail.com>
2024-01-08 03:22:45 +0900

    d

--- Assets/Scripts/JHS/Sword.cs
+++ Assets/Scripts/JHS/Sword.cs
@@ -9,16 +9,22 @@ public class Sword : MonoBehaviour
     public float MaxFarmingGauge;
     public float currentFarmingGauge;
 
-    [Header("Attack")]
+    [Header("State")]
     public float damage;
     public float attackSpeed;
+    public float swordHP;
+
+    [Header("Tendency")]
+    public float holyTendency;
+    public float evilTendency;
 
     public List<GameObject> monstersLockOnQueue= new List<GameObject>();
     public static Sword instance;
 
-    public bool attack;
-    private float time;
-    private GameObject playerAttackTarget;
+    private bool attack;
+    private bool onFaint;
+    private float attackDelayTime;
+    private float faintDurationTime;
     private void Awake()
     {
         if (instance == null) 
@@ -32,19 +38,35 @@ public class Sword : MonoBehaviour
         {
             AttackTarget();
         }
+        OnFaint();
     }
     public void AttackTarget()
     {
-        time += Time.deltaTime;
-        if (time > attackSpeed)
+        attackDelayTime += Time.deltaTime;
+        if (attackDelayTime > attackSpeed)
         {
-            time = 0;
+            attackDelayTime = 0;
             attack = true;
+        }
+    }
+    public void OnFaint()
+    {
+        if (swordHP <= 0)
+        {
+            onFaint = true;
         }
     }
     public void LockOnQueue(GameObject targetQueue)
     {
         monstersLockOnQueue.Add(targetQueue);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)//아무도 없을때 첫타를 위한 부분
+    {
+        if (collision.tag == "Monster" && collision.gameObject == monstersLockOnQueue[0])
+        {
+            collision.gameObject.GetComponent<MainSceneMonster>().hit = true;
+            collision.gameObject.GetComponent<MainSceneMonster>().monsterHP -= damage;
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
